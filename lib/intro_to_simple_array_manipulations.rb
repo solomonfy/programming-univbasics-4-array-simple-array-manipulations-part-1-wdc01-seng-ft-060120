ea73a4705dde753fd68b27b653879c32a2cd68c9
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-18 03:24:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 def using_push = ["colors_in_the_rainbow"]
-  using_push.push('next_color') = ["colors_in_the_rainbow", "next_color"]
+  using_push.push = ["colors_in_the_rainbow", "next_color"]
 end

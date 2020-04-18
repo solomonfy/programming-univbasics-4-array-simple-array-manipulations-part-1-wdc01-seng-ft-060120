3a773c5e7186747ce488dc05b0d023f0ca1d086e
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-18 03:10:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push = []
+  using_push.push ""
+end
+
+def using_unshift = []
+  using_unshift.unshift
+end
+
+def using_pop = []
+  using_pop.pop  
+end
+
+def pop_with_args = []
+  pop_with_args.  
+end
+
+def using_shift = []
+  using_shift.shift  
+end
+
+def shift_with_args
+  shift_with_args.
+  
+end

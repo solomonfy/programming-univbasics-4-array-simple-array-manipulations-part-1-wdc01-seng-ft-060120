6b37bdd1735eca57b6994dc7cc5b27d742bbe284
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-18 03:20:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,23 +3,4 @@ def using_push = ["colors_in_the_rainbow"]
 
 end
 
-def using_unshift = []
-  using_unshift.unshift
-end
 
-def using_pop = []
-  using_pop.pop
-end
-
-def pop_with_args = []
-  pop_with_args.
-end
-
-def using_shift = []
-  using_shift.shift
-end
-
-def shift_with_args
-  shift_with_args.
-
-end

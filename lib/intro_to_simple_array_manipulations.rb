ed089dd47ef3cabacd30a98ad7122c216c8d9e65
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-18 03:29:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,23 +2,3 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift = []
-  using_unshift.unshift
-end
-
-def using_pop = []
-  using_pop.pop
-end
-
-def pop_with_args = []
-  pop_with_args.
-end
-
-def using_shift = []
-  using_shift.shift
-end
-
-def shift_with_args
-  shift_with_args.
-
-end
